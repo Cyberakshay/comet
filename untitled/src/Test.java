8e3f83a6ed1b35d22413c9f20006b2b03fb159a8
Cyberakshay <akshaykamble5079@gmail.com>
2024-07-22 23:32:35 +0530

    init commit

---
+++ untitled/src/Test.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Test {
+    public static void main(String[] args){
+        String str = "Ajay is a java developer";
+        Map<Character, Integer> charCount = new HashMap<>();
+//        Arrays.asList(str.toCharArray()).stream().forEach(charr -> {
+//            if(charCount.containsKey(charr)){
+//                charCount.put
+//            }
+//
+//        });
+//        System.out.println(str.replaceAll(" ", "").chars().mapToObj(c -> (char)c).collect(Collectors.groupingBy(c -> Character.valueOf(c), Collectors.counting())));
+        System.out.println(str.replace("a", "k"));
+
+          ;
+    }
+
+}
